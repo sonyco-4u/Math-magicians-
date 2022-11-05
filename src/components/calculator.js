26eddff9c6eed65f0b62668f024f6d10f575b6e7
Ezema Anthony Sunday <tonyunday@gmail.com>
2022-11-05 00:26:20 -0700

    added some files

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,46 +1,50 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import '../calculator.css';
-import calculate from '../logic/calculate.js';
+import calculate from '../Logic/calculate';
+import './styles.css';
 
 const Calculator = () => {
-  const [total, setTotal] = useState(0);
-  const [next, setNext] = useState(null);
-  const [operation, setOperation] = useState(null);
+  const [count, setCount] = useState({ //count actualiza nuetsro componente y setcount es una funcion que podemos llamar cuando queremos actualizar usestate
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  const handleClick = (e) => {
-    const results = calculate({ total, next, operation }, e);
-    setTotal(results.total);
-    setNext(results.next);
-    setOperation(results.operation);
+  const onClickHandler = (e) => {
+    const data = e.target.innerHTML;
+    const result = calculate(count, data);
+    setCount(result);
   };
-  return (
 
-<div className="calculator">
-<div className="answer">{total}{operation}{next}</div>
-<div className="buttons">
-  <button type="button" id = "AC" className="button" onClick={() => handleClick('AC')}>AC</button>
-  <button type="button" id = "+/-" className="button" onClick={() => handleClick('+/-')}>+/-</button>
-  <button type="button" id = "%" className="button" onClick={() => handleClick('%')}>%</button>
-  <button type="button" id = "&divide;" className="button orange" onClick={() => handleClick('÷')}>&divide;</button>
-  <button type="button" id = "7" className="button" onClick={() => handleClick('7')}>7</button>
-  <button type="button" id = "8" className="button" onClick={() => handleClick('8')}>8</button>
-  <button type="button" id = "9" className="button" onClick={() => handleClick('9')}>9</button>
-  <button type="button" id = "&times;" className="button orange" onClick={() => handleClick('x')}>&times;</button>
-  <button type="button" id = "4" className="button" onClick={() => handleClick('4')}>4</button>
-  <button type="button" id = "5" className="button" onClick={() => handleClick('5')}>5</button>
-  <button type="button" id = "6" className="button" onClick={() => handleClick('6')}>6</button>
-  <button type="button" id = "-" className="button orange" onClick={() => handleClick('-')}>-</button>
-  <button type="button" id = "1" className="button" onClick={() => handleClick('1')}>1</button>
-  <button type="button" id = "2" className="button" onClick={() => handleClick('2')}>2</button>
-  <button type="button" id = "3" className="button" onClick={() => handleClick('3')}>3</button>
-  <button type="button" id = "+" className="button orange" onClick={() => handleClick('+')}>+</button>
-  <button type="button" id = "0" className="button double-button" onClick={() => handleClick('0')}>0</button>
-  <button type="button" id = "." className="button" onClick={() => handleClick('.')}>.</button>
-  <button type="button" id = "=" className="button orange" onClick={() => handleClick('=')}>=</button>
-</div>
-</div>
+  const { total, next, operation } = count;
+  return (
+    <div className="container">
+      <div className="input">
+        {total}
+        {operation}
+        {next}
+      </div>
+      <div className="keypad">
+        <button onClick={onClickHandler} type="button" name="A/C">AC</button>
+        <button onClick={onClickHandler} type="button" name="+/-">+/-</button>
+        <button onClick={onClickHandler} type="button" name="%">%</button>
+        <button onClick={onClickHandler} type="button" id="divide" name="÷">÷</button>
+        <button onClick={onClickHandler} type="button" name="7">7</button>
+        <button onClick={onClickHandler} type="button" name="8">8</button>
+        <button onClick={onClickHandler} type="button" name="9">9</button>
+        <button onClick={onClickHandler} type="button" id="mult" name="*">x</button>
+        <button onClick={onClickHandler} type="button" name="4">4</button>
+        <button onClick={onClickHandler} type="button" name="5">5</button>
+        <button onClick={onClickHandler} type="button" name="6">6</button>
+        <button onClick={onClickHandler} type="button" id="less">-</button>
+        <button onClick={onClickHandler} type="button" name="1">1</button>
+        <button onClick={onClickHandler} type="button" name="2">2</button>
+        <button onClick={onClickHandler} type="button" name="3">3</button>
+        <button onClick={onClickHandler} type="button" id="plus" name="+">+</button>
+        <button onClick={onClickHandler} type="button" name=".">.</button>
+        <button onClick={onClickHandler} type="button" id="equal" name="=">=</button>
+        <button onClick={onClickHandler} type="button" id="cero" name="0" value="0">0</button>
+      </div>
+    </div>
   );
 };
-
 export default Calculator;
